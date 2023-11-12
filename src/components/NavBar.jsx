a8d114301602e9f8cf66060e33e5844adea63bc1
Souhardya20 <145491139+Souhardya20@users.noreply.github.com>
2023-11-12 12:59:32 +0530

    Add files via upload

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -72,7 +72,7 @@ const Navbar = (props) => {
                   className="block py-2 pl-3 pr-4 hover:scale-105 transition-transform rounded hover:bg-gray-100 md:hover:bg-transparent hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   onClick={clickAndScroll}
                   style={({ isActive }) =>
-                    isActive ? { color: "#00B8FF " } : { color: "white" }
+                    isActive ? { color: "rgb(2 240 254) " } : { color: "white" }
                   }
                 >
                   Home
@@ -85,7 +85,7 @@ const Navbar = (props) => {
                   className="block py-2 pl-3 pr-4 hover:scale-105 transition-transform rounded hover:bg-gray-100 md:hover:bg-transparent hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   onClick={clickAndScroll}
                   style={({ isActive }) =>
-                    isActive ? { color: "#00B8FF " } : { color: "white" }
+                    isActive ? { color: "rgb(2 240 254) " } : { color: "white" }
                   }
                 >
                   Gallery
@@ -97,7 +97,7 @@ const Navbar = (props) => {
                   className="block py-2 pl-3 pr-4 hover:scale-105 transition-transform rounded hover:bg-gray-100 md:hover:bg-transparent hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   onClick={clickAndScroll}
                   style={({ isActive }) =>
-                    isActive ? { color: "#00B8FF " } : { color: "white" }
+                    isActive ? { color: "rgb(2 240 254) " } : { color: "white" }
                   }
                 >
                   Team
@@ -109,7 +109,7 @@ const Navbar = (props) => {
                   to="/contact"
                   className="block py-2 pl-3 pr-4 hover:scale-105 transition-transform rounded hover:bg-gray-100 md:hover:bg-transparent hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   style={({ isActive }) =>
-                    isActive ? { color: "#00B8FF " } : { color: "white" }
+                    isActive ? { color: "rgb(2 240 254) " } : { color: "white" }
                   }
                 >
                   Contact
