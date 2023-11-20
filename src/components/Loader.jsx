43d8b1ce74803f9b55cca4cf00f26a871ad0a9cf
Souhardya20 <145491139+Souhardya20@users.noreply.github.com>
2023-11-21 00:41:32 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from "./assests/Logo.png"
 const Loader = () => {
   return (
-    <div className='container flex justify-center items-center bg-blue-900 h-[86vh] lg:h-[100vh]'>
+    <div className='flex justify-center items-center bg-blue-900 h-[86vh] lg:h-[100vh]'>
         <div className='loading'><img src={logo} className='w-[80px] h-[80px]' alt="" /></div>
     </div>
     
