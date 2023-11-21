5e751e9a6a8ddeea1e6d1fc8bda0ddb426c23177
Souhardya20 <145491139+Souhardya20@users.noreply.github.com>
2023-11-21 15:53:23 +0530

    Add files via upload

--- src/components/GoToTop.jsx
+++ src/components/GoToTop.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-
+// import React, { useEffect, useState } from "react";
+import React from "react";
 const GoToTop = () => {
   const scrollToTop = () => {
     window.scrollTo({
@@ -11,34 +11,35 @@ const GoToTop = () => {
     scrollToTop();
   };
 
-  const [show, setShow] = useState({
-    display:"none"
-  });
+  // const [show, setShow] = useState({
+  //   display: "none",
+  // });
 
-  const listenToScroll = () => {
-    let height = 100;
-    const winScroll =
-      document.body.scrollTop || document.documentElement.scrollTop;
-    if (winScroll >= height) {
-      setShow({display:"block"});
-    } else {
-      setShow({display:"none"});
-    }
-  };
-
-  useEffect(() => {
-    return () => {
-      window.addEventListener("scroll", listenToScroll);
-    };
-  }, []);
+  // const listenToScroll = () => {
+  //   let height = 100;
+  //   const winScroll =
+  //     document.body.scrollTop || document.documentElement.scrollTop;
+  //   if (winScroll >= height) {
+  //     setShow({ display: "block" });
+  //   } else {
+  //     setShow({ display: "none" });
+  //   }
+  // };
+  function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up");
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 100) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
+  }
+  window.addEventListener("scroll", scrollUp);
+  
 
   return (
-    
-      <div className="flex justify-center items-center" style={show}>
-        <div className="top-btn" onClick={clickAndScroll}>
-        <i class="fa-solid fa-arrow-up fa-beat"></i>
-        </div>
-      </div>
+    <div class="scrollup" id="scroll-up" onClick={clickAndScroll}>
+   
+  
+   <i class="uil uil-arrow-up scrollup__icon"></i>
+    </div>
     
   );
 };
